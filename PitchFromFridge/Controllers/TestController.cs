4e1060e04738640c19f65eee89572086ef60f430
Andranik <andranik.atoyan@symetri.com>
2021-06-13 19:39:51 +0200

    Project init

---
+++ PitchFromFridge/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PitchFromFridge.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/test")]
+        public string GetTest()
+        {
+            return "Success";
+        }
+
+        [HttpGet]
+        [Route("api/test2")]
+        public string GetTest2()
+        {
+            return "Success55";
+        }
+    }
+}
